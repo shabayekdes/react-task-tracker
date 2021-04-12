4db53802a6048b3dca3d03f96bd6350ea71ec7c4
Esmail Shabayek <esmail.shabayek@gmail.com>
2021-04-12 14:18:46 +0200

    Add button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import Button from "./Button";
 
 const Header = () => {
-    return (
-        <header>
-            <h1>Task Tracker</h1>
-        </header>
-    )
-}
+  const onClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <header>
+      <h1>Task Tracker</h1>
+      <Button
+        color="green"
+        text="Add"
+        onClick={onClick}
+      />
+    </header>
+  );
+};
 
-export default Header
+export default Header;
